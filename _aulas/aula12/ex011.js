b391e2beb3617fdbdbce7491f539b872e95f395b
tanislaucacuengue <tanislaucacuengue@gmail.com>
2025-08-27 13:53:22 +0100

    Criei o ex011

---
+++ _aulas/aula12/ex011.js
@@ -0,0 +1,11 @@
+var idade = 22
+
+if(idade < 16){
+    console.log("Não vota !!!")
+}
+ else if(idade < 18 ){
+        console.log("Voto opcional <!_!>")
+}
+else{
+    console.log("Voto obrigatorio --!!!!--")
+}
