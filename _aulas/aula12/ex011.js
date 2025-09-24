096b51109779530a9d20ea80a64cebb564a9bcf1
tanislaucacuengue <tanislaucacuengue@gmail.com>
2025-09-24 19:36:15 +0100

    fiz exercícios 11,12 e 13

--- _aulas/aula12/ex011.js
+++ _aulas/aula12/ex011.js
@@ -1,9 +1,9 @@
-var idade = 22
+var idade = 50
 
 if(idade < 16){
     console.log("Não vota !!!")
 }
- else if(idade < 18 ){
+ else if(idade < 18 || idade >= 68){
         console.log("Voto opcional <!_!>")
 }
 else{
---
+++ _aulas/aula12/ex013.js
@@ -0,0 +1,30 @@
+console.log("Digite um valor de 1 à 7")
+var semana = 3
+switch (semana) {
+    case semana == 1:
+        console.log("Domingo")
+        break;
+    case semana == 2:
+        console.log("2ª Feira")
+        break;
+    case semana == 3:
+        console.log("3ª Feira")
+        break;
+    case semana == 4:
+        console.log("4ª Feira")
+        break;
+    case semana == 5:
+        console.log("5ª Feira")
+        break;
+    case semana == 6:
+        console.log("6ª Feira")
+        break;
+    case semana == 7:
+        console.log("Sabado")
+        break;
+
+
+    default:
+        console.log("O valor não é valido")
+        break;
+}
